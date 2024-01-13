ad7420e3b4e0d40ccc0eaa6d19ef9ecaa4e361d9
Rafael Nieto <radikan.dev@gmail.com>
2024-01-13 19:33:18 +0100

    ADD AudioController methods + ErrorResponse

--- src/main/java/com/example/mymusic/mymusic/controllers/AudioController.java
+++ src/main/java/com/example/mymusic/mymusic/controllers/AudioController.java
@@ -1,61 +1,52 @@
 package com.example.mymusic.mymusic.controllers;
 
+import com.example.mymusic.mymusic.exceptions.ErrorResponse;
+import com.example.mymusic.mymusic.models.Audio;
+import com.example.mymusic.mymusic.services.AudioService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
-@RequestMapping("/api/exercises")
+@RequestMapping("/audio")
 public class AudioController {
 
-    private final ExerciseService exerciseService;
+    private final AudioService audioService;
 
-    public ExerciseController(ExerciseService exerciseService) {
-        this.exerciseService = exerciseService;
+    public AudioController(AudioService audioService) {
+        this.audioService = audioService;
     }
 
-    @PostMapping
-    public ResponseEntity<?> addExercise(@RequestBody Exercise exercise) {
-        return new ResponseEntity<>(exerciseService.addExercise(exercise), HttpStatus.CREATED);
+    @PostMapping("/create")
+    public ResponseEntity<?> saveAudio(@RequestBody Audio audio) {
+        return new ResponseEntity<>(audioService.saveAudio(audio), HttpStatus.CREATED);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> updateExercise(@PathVariable long id, @RequestBody Exercise updatedExercise) {
-        Exercise updated = exerciseService.updateExercise(id, updatedExercise);
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateAudio(@PathVariable long id, @RequestBody Audio updatedAudio) {
+        Audio updated = audioService.updateAudio(id, updatedAudio);
         if (updated != null) {
             return ResponseEntity.status(HttpStatus.OK).body(updated);
         } else {
-            ErrorResponse errorResponse = new ErrorResponse("User not found");
+            ErrorResponse errorResponse = new ErrorResponse("Audio not found");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
         }
     }
 
-    @GetMapping
-    public ResponseEntity<?> getAllExercises() {
-        return new ResponseEntity<>(exerciseService.getAllExercises(), HttpStatus.OK);
-    }
-    @GetMapping("/search/{name}")
-    public ResponseEntity<?> getExerciseByName(@PathVariable String name) {
-        return new ResponseEntity<>(exerciseService.getExerciseByName(name), HttpStatus.OK);
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> getAudiobyId(@PathVariable long id) {
+        return new ResponseEntity<>(audioService.getAudiobyId(id), HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Map<String, String>> deleteExercise(@PathVariable long id) {
-        exerciseService.deleteExercise(id);
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Exercise deleted successfully");
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    @GetMapping("/get/{name}")
+    public ResponseEntity<?> getAudiobyName(@PathVariable String name) {
+        return new ResponseEntity<>(audioService.getAudiobyName(name), HttpStatus.OK);
     }
 
-    @DeleteMapping("/deleteAll")
-    public ResponseEntity<Map<String, String>> deleteAllExercises() {
-        exerciseService.deleteAllExercises();
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "All exercises deleted successfully");
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteAudio(@PathVariable Long id) {
+        audioService.deleteAudio(id);
+        return new ResponseEntity<>("Audio deleted succesfully", HttpStatus.OK);
     }
 
 }
