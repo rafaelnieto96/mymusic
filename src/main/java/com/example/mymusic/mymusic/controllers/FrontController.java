f9cefc181b7a5122ed450232bdbff76c28ae1e96
Rafael Nieto <radikan.dev@gmail.com>
2024-01-22 21:56:34 +0100

    lil change

--- src/main/java/com/example/mymusic/mymusic/controllers/FrontController.java
+++ src/main/java/com/example/mymusic/mymusic/controllers/FrontController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Controller
 public class FrontController {
 
-    private AudioServiceImpl audioService;
+    private final AudioServiceImpl audioService;
 
     public FrontController(AudioServiceImpl audioService) {
         this.audioService = audioService;
